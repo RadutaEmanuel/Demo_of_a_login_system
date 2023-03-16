257361f28434bbef308883a641b6f8bca7589fee
RadutaEmanuel <112098762+RadutaEmanuel@users.noreply.github.com>
2023-03-16 10:55:19 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,166 +1,228 @@
-﻿using System;
-using System.Collections;
+using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Net.Mail;
 
-namespace exemple_stagiu1
+public class MailPass
 {
-    public class Program
+    public string email;
+    public string password;
+    public int wrongattempt = 0;
+}
+
+class Program
+{
+    private static bool IsValid(string email)
     {
-        static void Main(string[] args)
+        var valid = true;
+
+        try
         {
-            Console.WriteLine("Hello World!");
+            var emailAddress = new MailAddress(email);
+        }
+        catch
+        {
+            valid = false;
+        }
 
-            int i = 345;
-            float f = i;
+        return valid;
+    }
 
-            Console.WriteLine(i);
-            Console.WriteLine(f);
+    private static bool IsPass(string password)
+    {
+        int cnt = 0;
+        foreach (char c in password)
+        {
+            if (char.IsDigit(c) == true)
+                cnt++;
+        }
+        if (cnt == 0 || password.Length < 10)
+            return false;
+        else return true;
+    }
 
+    private static void WaitInput()
+    {
+        Console.WriteLine("Press anything to continue");
+        Console.ReadLine();
+    }
 
-             i = 100;
-            Console.WriteLine(i);
-            ChangeValue(i);
-            Console.WriteLine(i);
-
-            Student student1 = new Student();
-            student1.StudentName = "Bill";
-
-            Console.WriteLine(student1.StudentName);
-            ChangeReferenceType(student1);
-            Console.WriteLine(student1.StudentName);
-
-            Student student = new Student();
-
-            Console.WriteLine(student);
-
-            int nullableInt;
-            //Console.WriteLine(nullableInt);
-
-            var myAnonymousType = new
+    private static void IsAccountPassword(MailPass account, List<MailPass> blockedAccounts, List<MailPass> accounts) 
+    {
+        string password;
+        Console.Write("Password: ");
+        password = Console.ReadLine();
+        if (password != account.password)
+        {
+            account.wrongattempt++;
+            if (account.wrongattempt < 3)
             {
-                firstProperty = "First",
-                secondProperty = 2,
-                thirdProperty = true
-            };
-
-
-            Console.WriteLine(myAnonymousType.GetType().ToString());
-            Console.WriteLine(student.GetType().ToString());
-
-            i = 100;
-            string ceva = "Hello dynamic";
-            TestDymicType(i);
-            TestDymicType(ceva);
-            //ChangeValue(ceva);
-
-            dynamic dynamicVariable = 100;
-            Console.WriteLine("Dynamic variable value: {0}, Type: {1}", dynamicVariable, dynamicVariable.GetType().ToString());
-
-            dynamicVariable = "Hello World";
-            Console.WriteLine("Dynamic variable value: {0}, Type: {1}", dynamicVariable, dynamicVariable.GetType().ToString());
-
-            dynamicVariable = true;
-            Console.WriteLine("Dynamic variable value: {0}, Type: {1}", dynamicVariable, dynamicVariable.GetType().ToString());
-
-            dynamicVariable = DateTime.Now;
-            Console.WriteLine("Dynamic variable value: {0}, Type: {1}", dynamicVariable, dynamicVariable.GetType().ToString());
-
-            //string hello = "hellow world";
-            string hello = "hi eveybody";
-            hello = "altceva";
-
-            StringBuilder stringBUilderExample = new StringBuilder();
-            stringBUilderExample.AppendLine(hello);
-            stringBUilderExample.AppendLine("altceva");
-
-
-            i = 100;
-            string someExample = "Hellow example";
-
-            Console.WriteLine(someExample.GetType().ToString());
-            Console.WriteLine(someExample.GetType());
-
-            object someIntObjected = someExample;
-
-            Console.WriteLine(someIntObjected.GetType().ToString());
-
-            object intObjeted = 200;
-
-            int j = (int)intObjeted;
-            int k = (int)someIntObjected;
-
-
-            List<int> myIntList = new List<int>();
-            myIntList.Add(j);
-            //myIntList.Add(someExample);
-            Dictionary<int, string> keyValuePairs = new Dictionary<int, string>();
-
-            keyValuePairs.Add(i, someExample);
-
-
-            Console.WriteLine(keyValuePairs[i]);
-
-
-            myIntList = new List<int>();
-
-            var x = myIntList.ToArray();
-
-            Student studentul = new Student();
-            studentul.NumarMatricol = 1234;
-            studentul.NumarMatricol = 445;
-
-            ArrayList strArray = new ArrayList();
-            strArray.AddRange(new string[] { "first", "second", "third" });
-            strArray.Add("fourth");
-
-            foreach (string str in strArray)
-            {
-                Console.WriteLine("Entry:{0}", str);
+                Console.WriteLine("Wrong password !!! You have " + (3 - account.wrongattempt) + " attempts left");
+                IsAccountPassword(account, blockedAccounts, accounts);
             }
-
-            Console.ReadLine();
+            else
+            {
+                Console.WriteLine("Account blocked");
+                blockedAccounts.Add(account);
+                WaitInput();
+            }
+        }
+        else 
+        {
+            Console.Clear();
+            account.wrongattempt = 0;
+            Console.Write("Hello ");
+            foreach (char c in account.email) 
+            {
+                if (c == '@')
+                    break;
+                else Console.Write(c);
+            }
+            Console.WriteLine();
+            WaitInput();
         }
 
+    }
 
-
-        static void ChangeValue(int x)
+    private static void register(List<MailPass> accounts)
+    {
+        Console.Clear();
+        MailPass mp = new MailPass();
+        Console.WriteLine("Enter an email addres and a password that has at least 10 characters in length and contains at least 1 number");
+        Console.Write("Email: ");
+        mp.email = Console.ReadLine();
+        while (IsValid(mp.email) == false)
         {
-            x = 200;
-            Console.WriteLine(x);
+            Console.Clear();
+            Console.WriteLine("Invalid email! Try again!");
+            Console.Write("Email: ");
+            mp.email = Console.ReadLine();
         }
-
-        static void ChangeReferenceType(Student referinta)
+        Console.Write("Pasword: ");
+        mp.password = Console.ReadLine();
+        while (IsPass(mp.password) == false)
         {
-            referinta.StudentName = "Steve";
-            Console.WriteLine(referinta.StudentName);
+            Console.Clear();
+            Console.WriteLine("The password must have at least 10 characters in length and contain at least 1 number! Try again!");
+            Console.Write("Pasword: ");
+            mp.password = Console.ReadLine();
         }
+        accounts.Add(mp);
+        Console.WriteLine("Account succesfully registered");
+        WaitInput();
+    }
 
-        static void TestDymicType(dynamic someVariable)
+    private static void ChangePass(MailPass account, List<MailPass> accounts) 
+    {
+        string oldpass, newpass;
+        Console.WriteLine("Enter your old password");
+        Console.Write("Old password: ");
+        oldpass = Console.ReadLine();
+        if (oldpass == account.password)
         {
-            Console.WriteLine(someVariable);
+            Console.WriteLine("Success! Enter your new password");
+            Console.WriteLine("The password must have at least 10 characters in length and contain at least 1 number!");
+            Console.Write("New password: ");
+            newpass = Console.ReadLine();
+            while (IsPass(newpass) == false)
+            {
+                Console.Clear();
+                Console.WriteLine("The password must have at least 10 characters in length and contain at least 1 number! Try again!");
+                Console.Write("New pasword: ");
+                newpass = Console.ReadLine();
+            }
+            foreach (MailPass c in accounts)
+            {
+                if (c.email == account.email)
+                    c.password = newpass;
+            }
+            Console.WriteLine("Password changed successfully !");
+            WaitInput();
         }
-
-
-
-        public void Swap<T> (T parametru) where T : struct
+        else 
         {
-
+            Console.WriteLine("You don't know your old password! Please contact us.");
+            WaitInput();
         }
     }
 
-    public class Student
+    private static void ExistingAccount(List<MailPass> accounts, List<MailPass> blockedAccounts,int option)
     {
-        public string StudentName { get; set; }
-        public int NumarMatricol { get; set; }
-        public int NumarTelefon { get; set; }
+        Console.Clear();
+        string email;
+        int cnt = 0;
+        Console.WriteLine("Enter your email address");
+        Console.Write("Email: ");
+        email = Console.ReadLine();
+        foreach (MailPass account in accounts) 
+        {
+            if (email == account.email) 
+            {
+                cnt = 1;
+            }
+        }
+        foreach (MailPass blocked in blockedAccounts)
+        {
+            if (email == blocked.email)
+            {
+                Console.WriteLine("Account blocked");
+                cnt = 2;
+            }
+        }
+        switch (cnt)
+        {
+            case 0:
+                Console.WriteLine("The account dosen't exist try again or register");
+                WaitInput();
+                break;
+            case 2:
+                Console.WriteLine("The account is blocked try again or register again");
+                WaitInput();
+                break;
+            case 1:
+                foreach (MailPass account in accounts) 
+                {
+                    if (email == account.email) 
+                    {
+                        if (option == 2)
+                            IsAccountPassword(account, blockedAccounts, accounts);
+                        else if (option == 3)
+                            ChangePass(account, accounts);
+                    }
+                }
+                break;
+        }
     }
 
-
-    public class Student2
+    static void Main()
     {
-        public string Prenume { get; set; }
-        public int NumarMatricol { get; set; }
-        public int NumarPantofi { get; set; }
+        List<MailPass> accounts = new List<MailPass>();
+        List<MailPass> blockedAccounts = new List<MailPass>();
+        MailPass mp = new MailPass();
+        mp.email = "a@b.com";
+        mp.password = "asdfghjkl1";
+        accounts.Add(mp);
+        int option;
+        while (true)
+        {
+            Console.Clear();
+            Console.WriteLine("MENU \n Enter your option \n 1) Register \n 2) Login \n 3) Change password");
+            option = Convert.ToInt16(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    register(accounts);
+                    break;
+                case 2:
+                    ExistingAccount(accounts, blockedAccounts, option);
+                    break;
+                case 3:
+                    ExistingAccount(accounts, blockedAccounts, option);
+                    break;
+                default: 
+                    Console.WriteLine("The option does not exist");
+                    WaitInput();
+                    break;
+            }
+        }
     }
 }
